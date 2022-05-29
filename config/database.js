169e1a0e9b31db592179f818f3113981d538f611
dberkley3 <dberkley3@gmail.com>
2022-05-29 15:29:26 -0400

    Full Stack auth (class)

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mongodb+srv://savage:2121@cluster0.ufwe1.mongodb.net/?retryWrites=true&w=majority', 
+    'dbName': 'messages'
+};
